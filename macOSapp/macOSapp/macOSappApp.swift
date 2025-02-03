374748c9b0b504639987e38ae3236cc2a2e7834b
Erik Bershel <erik-bershel@github.com>
2025-02-03 20:11:02 +0100

    add sample project

---
+++ macOSapp/macOSapp/macOSappApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSappApp.swift
+//  macOSapp
+//
+//  Created by Erik Bershel on 3.2.25..
+//
+
+import SwiftUI
+
+@main
+struct macOSappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macOSapp/macOSappTests/macOSappTests.swift
@@ -0,0 +1,17 @@
+//
+//  macOSappTests.swift
+//  macOSappTests
+//
+//  Created by Erik Bershel on 3.2.25..
+//
+
+import Testing
+@testable import macOSapp
+
+struct macOSappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
